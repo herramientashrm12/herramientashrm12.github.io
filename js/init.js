b3a52060a3dc250986b90bbca6b382d636bd0c61
herramientasmari <herramientasmari@gmail.com>
2022-11-28 00:25:00 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBVdUOpq0rq7-Yhx2PsndWqERwPNthqj0c",
+  authDomain: "herramientashrm121.firebaseapp.com",
+  projectId: "herramientashrm121",
+  storageBucket: "herramientashrm121.appspot.com",
+  messagingSenderId: "927892690641",
+  appId: "1:927892690641:web:5493861c83d28233aa57f0",
+  measurementId: "G-TCMYR88PV6"
+});
